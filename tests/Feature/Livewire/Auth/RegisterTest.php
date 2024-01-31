3cfe284bf75078209a737026aee9476c9fea9000
Markley Mendes <markley.mendes@gmail.com>
2024-01-31 08:41:53 -0300

    CRM-1: test: it should be able to register a new in the system

--- tests/Feature/Livewire/Auth/RegisterTest.php
+++ tests/Feature/Livewire/Auth/RegisterTest.php
@@ -9,3 +9,20 @@ it('should render the component', function () {
     Livewire::test(Register::class)
         ->assertOk();
 });
+
+it('should be able to register a new in the system', function () {
+    Livewire::test(Register::class)
+        ->set('name', 'Joe Doe')
+        ->set('email', 'joe@email.com')
+        ->set('email_confirmation', 'joe@email.com')
+        ->set('password', 'password')
+        ->call('submit')
+        ->assertHasNoErrors();
+
+    assertDatabaseHas('users', [
+        'name'  => 'Joe Doe',
+        'email' => 'joe@email.com',
+    ]);
+
+    assertDatabaseCount('users', 1);
+});
