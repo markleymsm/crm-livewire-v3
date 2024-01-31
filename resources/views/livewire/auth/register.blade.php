45d0b1dce2e08ec837288bfb476687719faf2546
Markley Mendes <markley.mendes@gmail.com>
2024-01-31 10:16:00 -0300

    CRM-1: feat: register form front-end

--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -1,3 +1,13 @@
-<div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
-</div>
+<x-card shadow class="mx-auto w-[350px]">
+    <x-form wire:submit="submit">
+        <x-input label="Name" wire:model="name" />
+        <x-input label="Email" wire:model="email" />
+        <x-input label="Confirm your email" wire:model="email_confirmation" />
+        <x-input label="Password" wire:model="password" type="password" />
+
+        <x-slot:actions>
+            <x-button label="Reset" type="reset" />
+            <x-button label="Register" class="btn-primary" type="submit" spinner="submit" />
+        </x-slot:actions>
+    </x-form>
+</x-card>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Livewire\Auth\Register;
 use App\Livewire\Welcome;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', Welcome::class);
+Route::get('/register', Register::class)->name('auth.register');
+Route::get('/logout', fn () => Auth::logout());
