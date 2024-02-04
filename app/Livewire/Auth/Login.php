25a50cd2185c11a391ed1bd24d7619788d75970e
Markley Mendes <markley.mendes@gmail.com>
2024-02-04 09:50:48 -0300

    CRM-2: feat: adding RateLimiter on login to many attempts

--- app/Livewire/Auth/Login.php
+++ app/Livewire/Auth/Login.php
@@ -3,14 +3,15 @@
 namespace App\Livewire\Auth;
 
 use Illuminate\Contracts\View\View;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\{Auth, RateLimiter};
+use Illuminate\Support\Str;
 use Livewire\Component;
 
 class Login extends Component
 {
-    public string $email = '';
+    public ?string $email = null;
 
-    public string $password = '';
+    public ?string $password = null;
 
     public function render(): View
     {
@@ -20,15 +21,39 @@ class Login extends Component
     public function tryToLogin(): void
     {
 
+        if ($this->ensureIsNotRateLimiting()) {
+            return;
+        }
+
         if (!Auth::attempt([
             'email'    => $this->email,
             'password' => $this->password,
         ])) {
+            RateLimiter::hit($this->throttleKey());
+
             $this->addError('invalidCredentials', trans('auth.failed'));
 
             return;
         }
 
         $this->redirect(route('dashboard'));
+    }
+
+    private function throttleKey(): string
+    {
+        return Str::transliterate(Str::lower($this->email) . '|' . request()->ip());
+    }
+
+    private function ensureIsNotRateLimiting()
+    {
+        if (RateLimiter::tooManyAttempts($this->throttleKey(), 5)) {
+            $this->addError('rateLimiter', trans('auth.throttle', [
+                'seconds' => RateLimiter::availableIn($this->throttleKey()),
+            ]));
+
+            return true;
+        }
+
+        return false;
     }
 }
