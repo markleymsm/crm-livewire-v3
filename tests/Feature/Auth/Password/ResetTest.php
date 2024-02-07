e455f3e8d6037c0286ba61c63ba600ab696957b2
Markley Mendes <markley.mendes@gmail.com>
2024-02-07 19:14:39 -0300

    CRM-4: test: need to receive a valid token with a combination with the email

--- tests/Feature/Auth/Password/ResetTest.php
+++ tests/Feature/Auth/Password/ResetTest.php
@@ -2,30 +2,28 @@
 
 use App\Livewire\Auth\Password\{Recovery, Reset};
 use App\Models\User;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Support\Facades\{DB, Notification};
 use Livewire\Livewire;
 
 use function Pest\Laravel\get;
 
-test('i need to have a route that will receive the token and the email that needs to be reset it', function () {
-    get(route('password.reset'))
-        ->assertSeeLivewire('auth.password.reset');
-});
-
 test('need to receive a valid token with a combination with the email', function () {
+    Notification::fake();
+
     $user = User::factory()->create();
 
     Livewire::test(Recovery::class)
         ->set('email', $user->email)
         ->call('startPasswordRecovery');
 
-    $token = DB::table('password_reset_tokens')
-        ->where('email', '=', $user->email)
-        ->first();
+    Notification::assertSentTo($user, ResetPassword::class, function (ResetPassword $notification) {
+        get(route('password.reset') . '?token=' . $notification->token)
+            ->assertSuccessful();
 
-    get(route('password.reset'), ['token' => $token->token, 'email' => $token->email])
-        ->assertOk();
+        get(route('password.reset') . '?token=any-token')
+            ->assertRedirect(route('login'));
 
-    // get(route('password.reset'), ['token' => 'any-token', 'email' => $token->email])
-    // ->assertRedirect();
+        return true;
+    });
 });
