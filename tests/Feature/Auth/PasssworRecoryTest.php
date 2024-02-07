f5105475e63ab6310f068bda0a7c94fe47b1d204
Markley Mendes <markley.mendes@gmail.com>
2024-02-07 14:27:21 -0300

    CRM-4: test: adding new feature on the test needs to have a route to password recovery

--- tests/Feature/Auth/PasssworRecoryTest.php
+++ tests/Feature/Auth/PasssworRecoryTest.php
@@ -10,6 +10,7 @@ use function Pest\Laravel\get;
 
 test('needs to have a route to password recovery', function () {
     get(route('auth.password.recovery'))
+        ->assertSeeLivewire('auth.password.recovery')
         ->assertOk();
 });
 
