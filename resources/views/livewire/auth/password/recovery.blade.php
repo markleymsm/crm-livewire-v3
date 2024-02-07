de700217259c088beccf291c283e73ef87716437
Markley Mendes <markley.mendes@gmail.com>
2024-02-07 14:26:55 -0300

    CRM-4: feat: frontend of the password recovery component

--- resources/views/livewire/auth/password/recovery.blade.php
+++ resources/views/livewire/auth/password/recovery.blade.php
@@ -1,8 +1,23 @@
-<div>
+<x-card title="Password recovery" shadow class="mx-auto w-[450px]">
     @if ($message)
-        <span >
-            You will receive an email with the password recovery link.
-        </span>
-        
+        <x-alert icon="o-exclamation-triangle" class="alert-success mb-4">
+            <span>
+                You will receive an email with the password recovery link.
+            </span>
+        </x-alert>
     @endif
-</div>
+
+    <x-form wire:submit="startPasswordRecovery">
+        <x-input label="Email" wire:model="email" />
+        <x-slot:actions>
+            <div class="w-full flex items-center justify-between">
+                <a wire:navigate href="{{ route('login') }}" class="lin link-primary">
+                    Never mind, get back to login page.
+                </a>
+                <div>
+                    <x-button label="Submit" class="btn-primary btn-sm" type="submit" spinner="submit" />
+                </div>
+            </div>
+        </x-slot:actions>
+    </x-form>
+</x-card>
